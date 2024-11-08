9351655dcc1764c4cc65f6e30c84e6572f7af0f0
Hernan Araya <58577750+haraya@users.noreply.github.com>
2024-11-08 09:11:33 -0600

    Add files via upload

---
+++ 01_Variables_Operators/10-Calculate_Remainder.py
@@ -0,0 +1,14 @@
+"""
+Define two variables dividend and divisor with integer values.
+Calculate and print the remainder when dividend is divided by divisor.
+"""
+
+#Variables
+dividend = 15
+divisor = 3
+
+#Calculating
+result = dividend % divisor
+
+#Printing
+print(f"The result is: {round(result,2)}")
